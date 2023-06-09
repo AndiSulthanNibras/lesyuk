925e20ffc11a14a311a60177cebd44f0f7ded01f
ShineXD24 <akunkomputerya22@gmail.com>
2023-06-09 10:39:29 +0700

    Feat : [GW] Dashboard,Home,About

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GurubaruController;
+use App\Http\Controllers\MuridController;
+use App\Http\Controllers\API\ReviewController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/guru-baru', [GurubaruController::class, 'index'])->name('guru-baru');
+Route::get('/create-guru', [GurubaruController::class, 'create'])->name('create-guru');
+Route::post('/simpan-guru', [GurubaruController::class, 'store'])->name('simpan-guru');
+Route::get('/murids', [MuridController::class, 'index'])->name('murids');
+Route::get('/tambah-murid', [MuridController::class, 'create'])->name('tambah-murid');
+Route::post('/simpan-murid', [MuridController::class, 'store'])->name('simpan-murid');
+
+
+
+Route::get('/', [DashboardController::class, 'index']);
+Route::get('/home', function () {
+    return view('home');
 });
+Route::get('/guru', function () {
+    return view('list-guru');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/reviews', [ReviewController::class, 'home'])->name('reviews');
