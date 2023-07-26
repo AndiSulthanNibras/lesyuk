38b25fa27e3206dcb7f5cb6551af536329df043f
Andi Sulthan Nibras <evoszekt2437@gmail.com>
2023-07-26 09:16:10 +0700

    add: [AS] add page register

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
+use Illuminate\Http\Controllers\ProfileController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/register', function () {
+    return view('register/all');
+});
+
+Route::get('/login', function () {
+    return view('login/all');
+});
+
+Route::get('authenticated', [AuthenticationController::class, 'authenticated']);
+// Route::get('login', [AuthenticationController::class, 'login'])->name('login');
+Route::post('login', [AuthenticationController::class, 'login'])->name('login'  );
+Route::post('register', [AuthenticationController::class, 'register'])->name('register');
+
+Route::post('register', [AuthenticationController::class, 'register'])->name('register');
+Route::get('authenticated', [AuthenticationController::class, 'authenticated']);
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/profile/{id}', 'ProfileController@create')->name('profile.create');
+    Route::put('/profile/{id}', 'ProfileController@update')->name('profile.update');
+    Route::post('/profile/{id}', 'ProfileController@store')->name('profile.store');
+    Route::delete('/profile/{id}', 'ProfileController@delete')->name('profile.delete');
 });
